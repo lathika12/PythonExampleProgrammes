60ef27ba41630681e06a17b8b0832b16fbb1a3d9
lathika12 <lathika.tanu@gmail.com>
2018-03-18 21:55:05 +0530

    March18Commit1
    Example programs

---
+++ docstring.py
@@ -0,0 +1,8 @@
+def randommethod():
+	'''
+	The present is a random function defined
+	to understand the working of docstrings - 
+	or document strings in python.
+	'''
+	print ("Hi You called for randommethod? ")
+randommethod()
